37189f9b2c59a646ecef99a39c3e515205a85cc2
fishycat-boop <56757157+fishycat-boop@users.noreply.github.com>
2022-03-29 08:30:15 +0200

    neue Packages

---
+++ baku/API erstellen.py
@@ -0,0 +1,3 @@
+from baku.read_data.from_mi import return_heart_rate, summ
+print(return_heart_rate())
+print(summ(1,2))
---
+++ baku/read_data/from_mi.py
@@ -0,0 +1,4 @@
+def return_heart_rate():
+    return "hallo"
+def summ(a, b):
+    return a+b
