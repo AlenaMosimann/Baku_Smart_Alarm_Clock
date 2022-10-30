a1687240cb9415d06598823b291fd9a145512852
Alena Mosimann <56757157+AlenaMosimann@users.noreply.github.com>
2022-10-30 09:36:25 +0100

    Add files via upload

---
+++ Computer_Code/constants_version_2.py
@@ -0,0 +1,6 @@
+HOURS = [f'{x:02}' for x in range(24)] # logic from Andrew Clark
+MINUTES = [f'{x:02}' for x in range(60)] # logic from Andrew Clark
+DAYS = ["today", "tomorrow"]
+INITIAL_HOUR = "06"
+INITIAL_MINUTES = "30"
+INITIAL_DAY = "today"
